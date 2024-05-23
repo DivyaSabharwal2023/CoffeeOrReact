65154e0e364964d71a2c5efe20a839228ed2960a
Divya201199 <divyasabharwal2017@gmail.com>
2024-05-23 15:31:47 -0400

    react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Coffee from "./Coffee"
 function App() {
-  
-
+  const username="divya"
+   
   return (   // Adjacent jsx elements must be wrapped in an enclosing tag. Thats where fragmentation comes in.
                       // this is called fragment
-    <>
+    <>                       
     <Coffee/>
-    <h1> Coffee Or React</h1>
+    <h1> Coffee Or React {username} </h1>
     <p> Testing Fragmentation</p>
         
     </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+  return(
+    <div>
+      <h1> Custom App | Coffee</h1>
+    </div>
+  )
+}
+
+// const ReactElement={
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+
+// }
+
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const ReactElement=React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google'
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  <App />
+
 )
